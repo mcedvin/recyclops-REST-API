92b46fee638d9b20713e9201e18bf9737495fb48
mcedvin <edvin.of@gmail.com>
2019-05-10 12:42:26 +0200

    Initial commit

---
+++ src/main/java/com/recycling/Test/Service/UserService.java
@@ -0,0 +1,33 @@
+package com.recycling.Test.Service;
+
+import com.recycling.Test.Dao.FakeUserDao;
+import com.recycling.recycling.production.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+
+@Service
+public class UserService {
+
+    @Autowired
+//    @Qualifier("MySQL")
+    private FakeUserDao userDao;
+
+    public Collection<User> getAllUsers() {
+        return userDao.getAllUsers();
+    }
+    public User getUserById(int id){
+        return userDao.getUserById(id);
+    }
+    public void removeUserById(int id){
+        userDao.removeUserById(id);
+    }
+    public void updateUser(User user){
+        userDao.updateUser(user);
+    }
+    public void addUser(User user){
+        userDao.addUser(user);
+    }
+}
---
+++ src/main/java/com/recycling/recycling/production/User.java
@@ -0,0 +1,68 @@
+package com.recycling.recycling.production;
+
+
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "user")
+public class User implements Serializable {
+    @Column(name = "FirstName", length = 100)
+    private String firstName;
+    @Column(name = "LastName", length = 100)
+    private String lastName;
+    //    @GeneratedValue
+//    @GeneratedValue(generator = "system-uuid")
+//    @GenericGenerator(name = "system-uuid", strategy = "uuid")
+
+    @Column(name = "email", length = 100)
+    @Id
+    private String email;
+//    @OneToOne(optional = true)
+//    @JoinColumn(nullable = true, name = "UserAccount")
+//    private UserAccount userAccount; //TODO: dessa ska fixas
+
+    public User() {
+
+    }
+
+    public User(String firstName, String lastName, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+
+//    public UserAccount getUserAccount() {
+//        return userAccount;
+//    }
+//
+//    public void setUserAccount(UserAccount userAccount) {
+//        this.userAccount = userAccount;
+//    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
