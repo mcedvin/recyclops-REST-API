e03f9a7448a97af875a5c1da366e6cc1a7e35cf9
mcedvin <edvin.of@gmail.com>
2019-05-22 13:19:29 +0200

    travis CI

--- src/main/java/com/recycling/Rest/RecyclingApplication.java
+++ src/main/java/com/recycling/Rest/RecyclingApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.scheduling.annotation.EnableScheduling;
 //import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableJpaRepositories(basePackages = "com.recycling.DB.repository")
@@ -11,7 +12,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 //@EnableJpaRepositories(basePackages = "com.recycling.Rest.repository")
 
 //@ComponentScan(basePackages = {"com.recycling.Rest.Controller"})
-//@EnableScheduling
+@EnableScheduling
 @SpringBootApplication
 
 
